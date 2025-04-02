fcc032c1f7a376af56e953af0199f035be0f4a00
Antonio Oliveira <antoniovini47@gmail.com>
2025-04-02 18:56:58 -0300

    structure: changed home section components folders structure

---
+++ src/components/home-section-search/index.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import SearchBar from "@/components/search-bar";
+import SearchResult from "@/components/search-result";
+import type { SearchParams } from "@/types/search";
+
+export const SectionSearch = () => {
+  const [searchParams, setSearchParams] = useState<SearchParams | null>(null);
+
+  return (
+    <>
+      <SearchBar onSearchChanges={(data) => setSearchParams(data)} />
+      {searchParams && <SearchResult searchInput={searchParams} />}
+    </>
+  );
+};
