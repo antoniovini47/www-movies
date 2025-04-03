81236ba255dea0b9673abdeab80d7401f8383b94
Antonio Oliveira <antoniovini47@gmail.com>
2025-04-03 11:44:10 -0300

    feat: movie cards image clickable for opening movie details

--- src/components/card-movie/index.tsx
+++ src/components/card-movie/index.tsx
@@ -12,6 +12,7 @@ import type { Movie } from "@/types/movie";
 import { Badge } from "@/components/ui/badge";
 import { Skeleton } from "@/components/ui/skeleton";
 import { useState } from "react";
+import Link from "next/link";
 
 type CardMovieProps = {
   movie: Movie;
@@ -26,14 +27,16 @@ const CardMovie = ({ movie, className }: CardMovieProps) => {
       className={`relative group overflow-hidden rounded-lg shadow-md hover:shadow-xl transition-shadow duration-1000 ${className}`}>
       <CardHeader className="relative">
         {!isImageLoaded && <Skeleton className="w-full min-h-[300px] rounded-t-lg" />}
-        <img
-          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-          alt={movie.title}
-          className={`w-full min-h-2/3 rounded-t-lg transition-opacity duration-1000 ${
-            isImageLoaded ? "opacity-100" : "opacity-0"
-          } group-hover:scale-105 group-hover:brightness-90`}
-          onLoad={() => setIsImageLoaded(true)}
-        />
+        <Link href={`/movie/${movie.id}`} className="w-full h-full z-10">
+          <img
+            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+            alt={movie.title}
+            className={`w-full min-h-2/3 rounded-t-lg transition-opacity duration-1000 ${
+              isImageLoaded ? "opacity-100" : "opacity-0"
+            } group-hover:scale-105 group-hover:brightness-90`}
+            onLoad={() => setIsImageLoaded(true)}
+          />
+        </Link>
         <CardTitle className="absolute bottom-2 left-2 bg-black bg-opacity-50 text-white px-2 py-1 rounded-md text-sm transition-transform duration-1000 group-hover:-translate-y-1">
           <Badge variant="default" className="max-w-4/5 px-5">
             {movie.title}
