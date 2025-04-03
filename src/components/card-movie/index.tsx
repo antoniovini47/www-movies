e66408132850920a7d00737e53c1b844fe14cddb
Antonio Oliveira <antoniovini47@gmail.com>
2025-04-03 10:23:57 -0300

    style: use skeleton for loading movie posters on movie-card

--- src/components/card-movie/index.tsx
+++ src/components/card-movie/index.tsx
@@ -10,6 +10,8 @@ import {
 import RatingStars from "@/components/rating-stars";
 import type { Movie } from "@/types/movie";
 import { Badge } from "@/components/ui/badge";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useState } from "react";
 
 type CardMovieProps = {
   movie: Movie;
@@ -17,6 +19,8 @@ type CardMovieProps = {
 };
 
 const CardMovie = ({ movie, viewMode }: CardMovieProps) => {
+  const [isImageLoaded, setIsImageLoaded] = useState(false);
+
   return (
     <Card
       className={`${
@@ -25,10 +29,14 @@ const CardMovie = ({ movie, viewMode }: CardMovieProps) => {
           : `rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300`
       }`}>
       <CardHeader className="relative">
+        {!isImageLoaded && <Skeleton className="w-full min-h-[300px] rounded-t-lg" />}
         <img
           src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
           alt={movie.title}
-          className="w-full h-auto rounded-t-lg"
+          className={`w-full min-h-2/3 rounded-t-lg transition-opacity duration-500 ${
+            isImageLoaded ? "opacity-100" : "opacity-0"
+          }`}
+          onLoad={() => setIsImageLoaded(true)}
         />
         <CardTitle className="absolute bottom-2 left-2 bg-black bg-opacity-50 text-white px-2 py-1 rounded-md text-sm">
           <Badge variant="default" className="max-w-4/5 px-5">
